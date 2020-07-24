431ffd617ba5374078415ed388c751f0f4bcbd55
John Arundel <john@bitfieldconsulting.com>
2020-07-24 15:18:58 +0100

    Add doc comments

--- gmachine.go
+++ gmachine.go
@@ -1,3 +1,6 @@
+// Package gmachine implements a simple virtual CPU, known as the G-machine.
 package gmachine
 
+// DefaultMemSize is the number of 64-bit words of memory which will be
+// allocated to a new G-machine by default.
 const DefaultMemSize = 1024
