1b96dd03511297862f5bb0f0ebbb3a01e1cb8b56
John Arundel <john@bitfieldconsulting.com>
2020-12-22 14:41:41 +0000

    TestNew is passing

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,14 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+type Machine struct{
+	Memory []uint64
+	P uint64
+}
+
+func New() Machine {
+	return Machine{
+		Memory: make([]uint64, DefaultMemSize),
+	}
+}
