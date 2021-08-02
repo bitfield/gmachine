daaecc5a6fdea805af76a4347abe9d2d346f7697
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-08-02 10:11:14 -0300

    Up to section 4

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,36 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+const HALT = 0
+const NOOP = 1
+const INCA = 2
+const DECA = 3
+
+type GMachine struct {
+	A      uint64
+	P      uint64
+	Memory [DefaultMemSize]uint64
+}
+
+func New() *GMachine {
+	return &GMachine{
+		A:      0,
+		P:      0,
+		Memory: [1024]uint64{},
+	}
+}
+
+func (g *GMachine) Run() {
+	for _, slot := range g.Memory {
+		g.P++
+		switch slot {
+		case HALT:
+			return
+		case INCA:
+			g.A++
+		case DECA:
+			g.A--
+		}
+	}
+
+}
--- gmachine_test.go
+++ gmachine_test.go
@@ -22,4 +22,67 @@ func TestNew(t *testing.T) {
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
 	}
+	var wantA uint64 = 0
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
+	}
+}
+
+func TestHALT(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.Run()
+	var wantP uint64 = 1
+	if wantP != g.P {
+		t.Errorf("want initial P value %d, got %d", wantP, g.P)
+	}
+}
+
+func TestNOOP(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.Memory[0] = gmachine.NOOP
+	g.Run()
+	var wantP uint64 = 2
+	if wantP != g.P {
+		t.Errorf("want initial P value %d, got %d", wantP, g.P)
+	}
+}
+
+func TestINCA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.Memory[0] = gmachine.INCA
+	g.Run()
+	var wantA uint64 = 1
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
+	}
+}
+
+func TestDECA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.A = 2
+	g.Memory[0] = gmachine.DECA
+	g.Run()
+	var wantA uint64 = 1
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
+	}
+}
+
+func TestSubstract2From3(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.Memory[0] = gmachine.INCA
+	g.Memory[1] = gmachine.INCA
+	g.Memory[2] = gmachine.INCA
+	g.Memory[3] = gmachine.DECA
+	g.Memory[4] = gmachine.DECA
+	g.Run()
+	var wantA uint64 = 1
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
+	}
 }
